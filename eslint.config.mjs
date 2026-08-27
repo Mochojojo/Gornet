b3b9fd84777ed8a06ad87b97356bc6561829eabf
Novato <jesusivan6413@gmail.com>
2026-08-27 16:56:19 -0700

    niñato agrego otras cosas
    Signed-off-by: Novato <jesusivan6413@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    languageOptions: { globals: globals.browser } 
+  },
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]);
